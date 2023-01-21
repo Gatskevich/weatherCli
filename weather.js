4e750373d5719dcb788ab8caaed2bf5377a4ae25
rodionhatskevich <rodionhatskevich@gmail.com>
2023-01-21 01:30:22 +0100

    add api

--- weather.js
+++ weather.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js"
+import { getWeather } from './services/api.service.js';
 import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue, getKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
+    if (!token.length) {
+		printError("Don't transferred token");
+		return;
+	}
 	try {
-		await saveKeyValue('token', token);
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
 		printSuccess('Token save');
 	} catch (e) {
 		printError(e.message);
@@ -24,7 +29,7 @@ const initCLI = () => {
 	if (args.t) {
 		return saveToken(args.t);
 	}
-    // Show weather
+    getWeather('moscow');
 };
 
 initCLI();
