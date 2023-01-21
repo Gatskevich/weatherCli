fe74dc52c0dc927f7dbd2b9bbb0b3ba4b4830e6d
rodionhatskevich <rodionhatskevich@gmail.com>
2023-01-21 01:48:00 +0100

    add error processing and env

--- services/api.service.js
+++ services/api.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
 const getWeather = async (city) => {
-	const token = await getKeyValue(TOKEN_DICTIONARY.token);
+	const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
 	if (!token) {
 		throw new Error('API key not set, set it via command -t [API_KEY]');
 	}
